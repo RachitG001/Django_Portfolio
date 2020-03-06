ab5c4f2bdab3c2a7053486081accaf1cab52eca5
Rachit <rachitg747@gmail.com>
2020-03-06 11:01:51 +0530

    Home page done

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import Job
 # Create your views here.
+
+def home(request):
+    jobs = Job.objects.all()
+    print(jobs)
+    # for job in jobs.all:
+    #     print(job.image_file.url)
+    return render(request,'jobs/home.html',{'jobs':jobs})
